d13695b7cee138223402d9824050851b1ccd06ac
Omrigan <omrigann@gmail.com>
2016-05-05 00:13:54 +0700

    Misunderstanding

--- chatbot/views.py
+++ chatbot/views.py
@@ -23,7 +23,10 @@ def webhook(req):
                 m.sender = message['sender']['id']
                 m.text = message['message']['text']
                 m.save()
-                ask_question(m.sender)
+                if m.text.lower()=='ask':
+                    ask_question(m.sender)
+                else:
+                    misunderstood(m.sender)
     return HttpResponse()
     # print(dir(req))
     # m = models.Message()
@@ -51,8 +54,22 @@ def ask_question(user_id):
     print(r.text)
 
 
-def misunderstood():
-    pass
+def misunderstood(user_id):
+    question_req = {
+        'recipient': {
+            'id': user_id
+        },
+        'message': {
+            'text': 'Sorry, I didn\'t understand you :(. If you want me to ask question, type "Ask"'
+        }
+    }
+    url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % \
+      (
+          'EAAVZCyjFh4eEBAF6ox3WSbpiTUI4ksXPCTFLXpK2ZAeT7i85SgWM9aekq1eu8ZBMSCetTqObtl8DYWHPzRMGnYPT2ugwpD394mAZBonciOefHVxtZCiMsOKgYr6o3N5yKCbvuqVCV0Of6wRuoaY4UBH5AzjDPwUwntasI3jqGMgZDZD',)
+    response_msg = json.dumps(question_req)
+    r = requests.post(url, headers={"Content-Type": "application/json"}, data=response_msg)
+    print(r.text)
+
 
 
 def last_messages(req):
