4a7318fb0e8f382540375ec6f34e479bfa6b3b82
Omrigan <omrigann@gmail.com>
2016-05-04 23:55:39 +0700

    Ask question functionality

--- chatbot/views.py
+++ chatbot/views.py
@@ -3,14 +3,11 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from . import models
 import json
+import requests
 
 
 @csrf_exempt
 def webhook(req):
-    m = models.Message()
-    m.sender = '123'#str(req.body)
-    # m.text = message['message']['text']
-    m.save()
     # Post function to handle Facebook messages
     # Converts the text payload into a python dictionary
     incoming_message = json.loads(req.body)
@@ -26,12 +23,31 @@ def webhook(req):
                 m.sender = message['sender']['id']
                 m.text = message['message']['text']
                 m.save()
+                ask_question(m.sender)
     return HttpResponse()
     # print(dir(req))
     # m = models.Message()
     # m.sender = 'Oleg'
     # m.save()
-    return JsonResponse({'status': 'ok'})
+
+
+def ask_question(user_id):
+    question_req = {
+        'recipient': {
+            'id': user_id
+        },
+        'message': {
+            'text': 'Test'
+        }
+    }
+    url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % \
+          (
+              'EAAVZCyjFh4eEBAF6ox3WSbpiTUI4ksXPCTFLXpK2ZAeT7i85SgWM9aekq1eu8ZBMSCetTqObtl8DYWHPzRMGnYPT2ugwpD394mAZBonciOefHVxtZCiMsOKgYr6o3N5yKCbvuqVCV0Of6wRuoaY4UBH5AzjDPwUwntasI3jqGMgZDZD',)
+    requests.get(url, question_req)
+
+
+def misunderstood():
+    pass
 
 
 def last_messages(req):
